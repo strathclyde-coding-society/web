973c2d819fc1defb297eab92f6f716c046b4acc2
Kamil Zak <kamilzak00@gmail.com>
2022-08-15 14:48:37 +0100

    feat: add link to logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,9 +8,9 @@ function Nav() {
         <>
             <div className="sticky top-0 z-50 max-h-1/6 w-screen flex flex-row justify-between py-2 px-20 bg-opacity-80 bg-gray-750 border-gray-550 backdrop-blur">
             <div className="flex mr-5 h-full text-xs">
-                <a href="">
+                <Link to="/">
                     <img src={logo} className="w-16 h-16"></img>
-                </a>
+                </Link>
             </div>
             <div className="flex flex-row h-full">
                 <ul className="flex flex-row justify-center items-center space-x-16 text-gray-50">
@@ -20,7 +20,7 @@ function Nav() {
                     <li><Link to="/team" className="hover:text-gray-400 transition duration-300 ease-in-out">Meet the Committee</Link></li>
                 </ul>
             </div>
-            <div class="flex flex-row justify-start items-center">
+            <div className="flex flex-row justify-start items-center">
                 <a href="https://www.strathunion.com/groups/find-a-society/society/coding/" target="_blank" rel="noopener" aria-label="strathclyde coding society">
                     <button className="text-sm text-white font-semibold flex justify-center items-center pt-[10px] pb-[10px] pl-[15px] pr-[15px] rounded-md opacity-70 hover:opacity-100 transition duration-300 ease-in-out bg-gradient-to-r from-blue-700 to-purple-700">
                         Join today!
