45796cb336e58f3d87a692241c1a7ddab9984cd6
Kamil Zak <kamilzak00@gmail.com>
2022-08-15 15:00:50 +0100

    feat: modify existing background proportions

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,7 +1,7 @@
 function Background() {
     return (
         <div className="relative">
-            <div className="bg-gradient overflow-hidden"></div>
+            <div className="bg-gradient overflow-hidden h-screen w-screen scale-150"></div>
         </div>
     )
 }
