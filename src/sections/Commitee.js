ce06be307745140da8acfaab61fd8abbd78ad6d9
hamada.alosh <mohamad.75.alosh@gmail.com>
2023-01-05 19:13:18 +0000

    changed members

--- src/sections/Commitee.js
+++ src/sections/Commitee.js
@@ -1,19 +1,30 @@
 import React from 'react';
 
 const members = [
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
-    {name: "Lorem ipsum", role: "Lorem, ipsum.", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", github: "#", linkedin: "#", twitter: "#"},
+    {name: "Hamada Alosh", role: "President", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, est?", links:[{link: "https://www.linkedin.com/in/hamada-alosh/", icon: "bi bi-linkedin"}]},
+    {name: "Ayushi Badiyani", role: "Vice President", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, est?", links:[{link: "https://www.linkedin.com/in/ayushi-badiyani/", icon: "bi bi-linkedin"}]},
+    {name: "Cameron Bell", role: "Technical Officer", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, est?", links:[{link: "https://www.linkedin.com/in/bell-cameron/", icon: "bi bi-linkedin"}]},
+    {name: "Shelly Master", role: "Treasurer", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, est?", links:[{link: "https://www.linkedin.com/in/shelly-master-47b89b185/", icon: "bi bi-linkedin"}]},
+    {name: "Charles Watson", role: "Events Coordinator", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, est?", links:[{link: "https://www.linkedin.com/in/charles-watson-3b7903189/", icon: "bi bi-linkedin"}]},
+    {name: "Kelechi Nwokeabia", role: "Events Coordinator", img: "https://www.zooborns.com/.a/6a010535647bf3970b02a2eec93591200d-800wi", description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, est?", links:[{link: "https://www.linkedin.com/in/kelechi-nwokeabia-657013247/", icon: "bi bi-linkedin"}]},
 ];
 
 function Committee() {
+    function Links (props) {
+        console.log(props)
+        return(
+            props.pairs.map(pair =>{
+                return(
+                    <div>
+                    <a href={pair.link}>
+                    <i className={pair.icon}></i>
+                    </a>
+                    </div>
+                )
+            })
+        )
+    }
+
     const Members = () => {
         return( 
             members.map(member => {
@@ -24,11 +35,9 @@ function Committee() {
                             <div className="flex-grow md:p-4">
                                 <h2 className="title-font font-medium text-lg text-slate-50">{member.name}</h2>
                                 <h3 className="text-gray-500 mb-3 text-sm">{member.role}</h3>
-                                <p className="mb-4 hidden md:block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, est?</p>
+                                <p className="mb-4 hidden md:block">{member.description}</p>
                                 <div className="inline-flex">
-                                    <i className="bi bi-twitter"></i>
-                                    <i className="bi bi-twitter ml-1"></i>
-                                    <i className="bi bi-twitter ml-1"></i>
+                                    <Links pairs={member.links}/>
                                 </div>
                             </div>
                         </div>
