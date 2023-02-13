568b49a37acfab2ee9ba7165ee053a2de1a69bf4
hamada.alosh <mohamad.75.alosh@gmail.com>
2023-02-13 17:58:46 +0000

    changed colour to purple

--- src/sections/About.js
+++ src/sections/About.js
@@ -12,7 +12,7 @@ class About extends React.Component {
                                     <img className="rounded" src="https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
                                 </div>
                                 <div className="md:7/12 lg:w-6/12">
-                                    <h2 className="text-xs text-indigo-600 tracking-widest font-medium title-font mb-1">Lorem ipsum dolor sit amet.</h2>
+                                    <h2 className="text-xs text-indigo-600 tracking-widest font-medium title-font mb-1">Compotors are kewl.</h2>
                                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-slate-50">Who are we?</h1>
                                     <p className="mt-6 leading-relaxed text-slate-400">The Strathclyde Coding Society is one of the biggest societies at strathclyde - a student led body that aims to encourage students from all backgrounds to increase their coding skills, we do this by hosting workshops and social events to help students connect with each other and industry!</p>
                                     <p className="mt-6 leading-relaxed text-slate-400">The Strathclyde Coding welcomes everyone regardless of their background, gender or religion. If you are a woman or a member of the LGBTQ+ community consider also joining <a href="https://www.strathunion.com/groups/find-a-society/society/strathwit/" className="text-indigo-600">StrathWIT</a></p>
@@ -23,12 +23,12 @@ class About extends React.Component {
                                             <a className="hover:scale-125 transition ease-in-out delay-50" href="https://www.instagram.com/strathcoding/" target="_blank" rel="noopener noreferrer"><i className="bi bi-instagram ml-1 px-1"></i></a>
                                             <a className="hover:scale-125 transition ease-in-out delay-50"href="https://www.linkedin.com/company/strathcoding/" target="_blank" rel="noopener noreferrer"><i className="bi bi-linkedin ml-1 px-1"></i></a>
                                         </div>
-                                        <div className="flex flex-col justify-center items-center p-3">
-                                            <div className="flex space-x-3 items-center">
-                                                <span className="text-6xl sm:text-8xl font-bold">400</span>
+                                        <div className="flex flex-col justify-center items-center p-3 text-indigo-600">
+                                            <div className="flex space-x-3 items-center ">
+                                                <span className="text-6xl sm:text-8xl font-bold">200</span>
                                                 <span className="text-2xl sm:text-4xl font-bold">+</span>
                                             </div>
-                                            <p>Members</p>
+                                            <p className="text-indigo-500">Members</p>
                                         </div>
                                     </div>
                                 </div>
